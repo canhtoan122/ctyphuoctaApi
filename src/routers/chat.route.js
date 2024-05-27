3ccd8aedd853799a80fc1d1b03aa7a40cd49d35d
canhtoan122 <74394729+canhtoan122@users.noreply.github.com>
2024-05-27 15:59:30 +0700

    Update chat bot

--- src/routers/chat.route.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const chatController = require("../controllers/chat.controller.js");
-const chatRouter = express.Router();
-
-chatRouter.post("/", (req, res) =>{
-    chatController.chat(req, res);
-});
-module.exports = chatRouter;
